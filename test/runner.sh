e3f531c7c12c0fe0e39d79dbb68794c67df71672
Robin Bate Boerop <me@robinbb.com>
2019-01-13 15:19:00 -0800

    Make -no-ssh and -minimal variants (#6)

--- test/runner.sh
+++ test/runner.sh
@@ -16,22 +16,33 @@ function cleanup()
 
 PWD=`pwd`
 
-echo " --> Starting container"
-ID=$( docker run -d -p 22 -v $PWD/test:/test $NAME:$VERSION /sbin/my_init )
+echo " --> Starting container $NAME:$VERSION"
+ID=$(
+  docker run -d -p 22 \
+    -v $PWD/test:/test \
+    --env DISABLE_SYSLOG \
+    --env DISABLE_SSH \
+    --env DISABLE_CRON \
+    $NAME:$VERSION \
+    /sbin/my_init )
 sleep 1
 
-echo " --> Obtaining SSH port number"
-SSHPORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' "$ID"`
-if [[ "$SSHPORT" = "" ]]; then
-	abort "Unable to obtain container SSH port number"
+if [ "$DISABLE_SSH" -eq 0 ] ; then
+  echo " --> Obtaining SSH port number"
+  SSHPORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "22/tcp") 0).HostPort}}' "$ID"`
+  if [[ "$SSHPORT" = "" ]]; then
+    abort "Unable to obtain container SSH port number"
+  fi
 fi
 
 trap cleanup EXIT
 
-echo " --> Enabling SSH in the container"
-docker exec -t -i $ID /etc/my_init.d/00_regen_ssh_host_keys.sh -f
-docker exec -t -i $ID rm /etc/service/sshd/down
-docker exec -t -i $ID sv start /etc/service/sshd
+if [ "$DISABLE_SSH" -eq 0 ] ; then
+  echo " --> Enabling SSH in the container"
+  docker exec -t -i $ID /etc/my_init.d/00_regen_ssh_host_keys.sh -f
+  docker exec -t -i $ID rm /etc/service/sshd/down
+  docker exec -t -i $ID sv start /etc/service/sshd
+fi
 
 echo " --> Running tests in container"
 sleep 1 # Give container some more time to start up.
